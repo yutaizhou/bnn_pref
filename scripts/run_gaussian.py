931c8e68aa6c9b49be8b67b1f17f35bbce1405cd
Yutai Zhou <yutaizh@gmail.com>
2025-02-27 13:30:14 -0800

    gaussian example

--- scripts/run_gaussian.py
+++ scripts/run_gaussian.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from functools import partial
 
 import arviz as az
@@ -10,17 +11,20 @@ import matplotlib.pyplot as plt
 from bnn_pref.alg.mcmc import build_hmc, build_mh, plot_samples, run_mcmc
 
 
+@dataclass
 class BimodalGaussian:
+    var: float = None
+
     @staticmethod
-    def sample(key, n_samples, loc1, loc2, var, weight=0.5):
+    def sample(key, n_samples, mean1, mean2, var, weight=0.5):
         key1, key2, key3 = jr.split(key, 3)
 
         # Sample from uniform to decide which mode to sample from
         mode_selector = jr.uniform(key1, (n_samples, 1)) < weight
 
         # Sample from both modes
-        samples1 = loc1 + jr.normal(key2, (n_samples, 1)) * jnp.sqrt(var)
-        samples2 = loc2 + jr.normal(key3, (n_samples, 1)) * jnp.sqrt(var)
+        samples1 = mean1 + jr.normal(key2, (n_samples, 1)) * jnp.sqrt(var)
+        samples2 = mean2 + jr.normal(key3, (n_samples, 1)) * jnp.sqrt(var)
 
         # Select samples based on the mode selector
         samples_ND = jnp.where(mode_selector, samples1, samples2)
@@ -28,16 +32,21 @@ class BimodalGaussian:
         return samples_ND
 
     @staticmethod
-    def logpdf(x, loc1, loc2, logvar, weight=0.5):
-        var = jnp.exp(logvar)
+    def logpdf(x, mean1, mean2, var, weight=0.5):
         std = jnp.sqrt(var)
-        mode1 = jsp.stats.norm.logpdf(x, loc=loc1, scale=std) + jnp.log(weight)
-        mode2 = jsp.stats.norm.logpdf(x, loc=loc2, scale=std) + jnp.log(1 - weight)
+        mode1 = jsp.stats.norm.logpdf(x, loc=mean1, scale=std) + jnp.log(weight)
+        mode2 = jsp.stats.norm.logpdf(x, loc=mean2, scale=std) + jnp.log(1 - weight)
         return jnp.logaddexp(mode1, mode2)
 
-    @staticmethod
-    def potential(params, data):
-        return BimodalGaussian.logpdf(data, **params).sum()
+    def potential(self, params, data):
+        if "logvar" in params:
+            var = jnp.exp(params["logvar"])
+        else:
+            var = self.var
+        mean1 = params["mean1"]
+        mean2 = params["mean2"]
+
+        return BimodalGaussian.logpdf(x=data, var=var, mean1=mean1, mean2=mean2).sum()
 
 
 if __name__ == "__main__":
@@ -46,8 +55,8 @@ if __name__ == "__main__":
 
     data_kwargs = {
         "n_samples": 1000,
-        "loc1": 5,
-        "loc2": -5,
+        "mean1": 5,
+        "mean2": -5,
         "var": 1,
     }
     key, data_key = jr.split(key)
@@ -56,28 +65,28 @@ if __name__ == "__main__":
     fig, ax = plt.subplots(2, 1)
     plot_samples(ax[0], data, label="Data", range=(-10, 10))
 
-    # init_samples = {"loc1": 0, "loc2": 0, "logvar": 1.0}
-    init_samples = {"loc1": 4, "loc2": -4, "logvar": 1.0}
+    # init_samples = {"mean1": 0, "mean2": 0, "logvar": 1.0}
+    init_samples = {"mean1": 4, "mean2": -4}
 
     step_size = 0.1
     sigma = jnp.ones(len(init_samples)) * step_size
     alg = build_mh(partial(dist.potential, data=data), sigma)
 
     mcmc_kwargs = {
-        "init_samples": init_samples,
+        "init_sample": init_samples,
         "n_samples": 50000,
         "burn_in": 10000,
         "thinning": 2,
     }
 
     key, mcmc_key = jr.split(key)
-    samples = run_mcmc(key=mcmc_key, alg=alg, **mcmc_kwargs)
+    samples, states, infos = run_mcmc(key=mcmc_key, alg=alg, **mcmc_kwargs)
 
-    key, *samples_key = jr.split(key, 1 + len(samples["loc1"]))
+    key, *samples_key = jr.split(key, 1 + len(samples["mean1"]))
     learned_samples = jax.vmap(partial(dist.sample, n_samples=1))(
         key=jnp.asarray(samples_key),
-        loc1=samples["loc1"],
-        loc2=samples["loc2"],
+        mean1=samples["mean1"],
+        mean2=samples["mean2"],
         var=jnp.exp(samples["logvar"]),
     ).squeeze(axis=1)
     print(learned_samples.shape)
@@ -86,9 +95,9 @@ if __name__ == "__main__":
     true_pdf = jnp.exp(
         dist.logpdf(
             x=x,
-            loc1=data_kwargs["loc1"],
-            loc2=data_kwargs["loc2"],
-            logvar=jnp.log(data_kwargs["var"]),
+            mean1=data_kwargs["mean1"],
+            mean2=data_kwargs["mean2"],
+            var=data_kwargs["var"],
         )
     )
     plot_samples(
---
+++ scripts/test_gaussian.py
@@ -0,0 +1,157 @@
+from functools import partial
+
+import arviz as az
+import blackjax
+import jax
+import jax.numpy as jnp
+import jax.random as jr
+import jax.scipy as jsp
+import matplotlib.pyplot as plt
+import numpy as np
+from run_gaussian import BimodalGaussian
+
+# # Parameters for the bimodal Gaussian
+# mean1 = -2.0  # Mean of the first Gaussian
+# mean2 = 3.0  # Mean of the second Gaussian
+# shared_variance = 1.0  # Fixed shared variance
+# weight = 0.5  # Equal weight for both Gaussians
+
+# # Number of samples
+# total_samples = 1000
+
+# # Generate data
+# samples1 = np.random.normal(
+#     mean1, np.sqrt(shared_variance), int(total_samples * weight)
+# )
+# samples2 = np.random.normal(
+#     mean2, np.sqrt(shared_variance), int(total_samples * weight)
+# )
+# data = np.concatenate([samples1, samples2])
+
+# # Shuffle the data
+# np.random.shuffle(data)
+
+
+# Plot the data
+# plt.hist(data, bins=50, density=True, alpha=0.6, color="g")
+# plt.title("Histogram of Bimodal Gaussian Data")
+# plt.xlabel("Value")
+# plt.ylabel("Density")
+# plt.show()
+
+
+# # Define the log-likelihood function for the bimodal Gaussian
+# def log_likelihood(params, data):
+#     mean1, mean2 = params
+#     variance = shared_variance  # Fixed shared variance
+
+#     # Compute the likelihood for each Gaussian
+#     likelihood1 = jax.scipy.stats.norm.logpdf(data, mean1, jnp.sqrt(variance))
+#     likelihood2 = jax.scipy.stats.norm.logpdf(data, mean2, jnp.sqrt(variance))
+
+#     # Combine the likelihoods with equal weights
+#     combined_likelihood = jnp.logaddexp(likelihood1, likelihood2) - jnp.log(2)
+#     return jnp.sum(combined_likelihood)
+
+
+# # Define the log-prior function (assume flat priors)
+# def log_prior(params):
+#     return 0.0  # Flat prior
+
+
+# # Define the log-posterior function
+# def log_posterior(params, data):
+#     return log_prior(params) + log_likelihood(params, data)
+
+
+# # Wrap the log-posterior for BlackJAX
+# def potential(params, data):
+#     return log_posterior(params, data)
+
+
+def inference_loop(rng_key, kernel, initial_state, num_samples):
+    @jax.jit
+    def one_step(state, rng_key):
+        state, _ = kernel(rng_key, state)
+        return state, state
+
+    keys = jax.random.split(rng_key, num_samples)
+    _, states = jax.lax.scan(one_step, initial_state, keys)
+
+    return states
+
+
+if __name__ == "__main__":
+    data_kw = {
+        "mean1": -2.0,
+        "mean2": 3.0,
+        "var": 1.0,
+        "weight": 0.5,
+        "n_samples": 1000,
+    }
+
+    key = jr.key(0)
+    dist = BimodalGaussian()
+    key, data_key = jr.split(key)
+    data = dist.sample(data_key, **data_kw)
+
+    # Initialize parameters and sampler
+    initial_params = {"mean1": 0.0, "mean2": 0.0}
+    rng_key = jax.random.PRNGKey(42)
+    inv_mass_matrix = np.array([0.5, 0.01])
+    step_size = 1e-3
+
+    sampler = blackjax.nuts(
+        partial(dist.potential, data=data, fixed_logvar=jnp.log(data_kw["var"])),
+        step_size,
+        inv_mass_matrix,
+    )
+
+    mcmc_kw = {
+        "init_sample": initial_params,
+        "n_samples": 2000,
+        "burn_in": 1000,
+        "thinning": 2,
+    }
+
+    keys, subkey = jax.random.split(rng_key)
+    kernel = sampler.step
+    state = sampler.init(initial_params)
+    states = inference_loop(subkey, kernel, state, mcmc_kw["n_samples"])
+    states_sampling = jax.tree.map(
+        lambda x: x[mcmc_kw["burn_in"] :: mcmc_kw["thinning"]],
+        states,
+    )
+    posterior_samples = states_sampling.position
+    posterior_samples = jnp.stack(
+        [posterior_samples["mean1"], posterior_samples["mean2"]], axis=1
+    )
+
+    # Sort the means in each sample so mean1 is always less than mean2
+    sorted_samples = jnp.sort(posterior_samples, axis=1)
+
+    # Convert to ArviZ InferenceData format with sorted samples
+    idata = az.from_dict(
+        posterior={
+            "mean1": jnp.expand_dims(sorted_samples[:, 0], 0),  # Smaller mean
+            "mean2": jnp.expand_dims(sorted_samples[:, 1], 0),  # Larger mean
+        }
+    )
+
+    # Plot posterior distributions with ArviZ
+    az.plot_posterior(
+        idata,
+        var_names=["mean1", "mean2"],
+        ref_val=[data_kw["mean1"], data_kw["mean2"]],
+        hdi_prob=0.94,
+    )
+    plt.show()
+
+    # Trace plots to check convergence of chains
+    plt.figure()
+    az.plot_trace(idata)
+    plt.show()
+
+    # Summary statistics (means, HDIs, etc.)
+    summary_stats = az.summary(idata, hdi_prob=0.94)
+    print(summary_stats)
