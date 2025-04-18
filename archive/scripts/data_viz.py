66a01ea91c3226f876d389e6c48aa30d8b31c0b5
Yutai Zhou <yutaizh@gmail.com>
2025-04-18 15:44:53 -0700

    data viz

--- archive/scripts/data_viz.py
+++ archive/scripts/data_viz.py
@@ -23,6 +23,7 @@ def main(cfg):
     """
     Plot histogram of returns for each task
     """
+    n_bins = 10
     seed = get_random_seed() if cfg["seed"] == -1 else cfg["seed"]
     key = jr.key(seed)
 
@@ -44,6 +45,11 @@ def main(cfg):
 
     fig, axs = plt.subplots(4, 4, figsize=(12, 8))  # 13 tasks total
     axs = axs.flatten()
+
+    # Create empty lists to store legend lines and labels
+    lines = []
+    labels = []
+
     for i, task in enumerate(tasks):
         # * update cfg
         new_cfg = hydra.compose("config", overrides=[f"task={task}"])
@@ -57,15 +63,33 @@ def main(cfg):
         train_returns = data_dict["train_trajs"]["returns"]
         train_prefs = data_dict["train_prefs"]
         n_trajs = train_returns.shape[0]
-        print(f"{task}: {n_trajs} trajectories, {train_prefs.n_mislabels} mislabels")
+        n_queries = train_prefs.queries_Q2.shape[0]
+        n_mislabels = train_prefs.n_mislabels
+        print(f"{task:13}: {n_trajs} trajs, {n_mislabels} / {n_queries} mislabels")
+
+        # * plot histogram
         ax = axs[i]
-        ax.hist(train_returns, bins=50, edgecolor="black")
+        ax.hist(train_returns, bins=n_bins, edgecolor="black")
         ax.set_title(f"{task} ({n_trajs} trajs)")
         ax.set_xlabel("Return")
         ax.set_ylabel("Frequency")
 
+        mean = jnp.mean(train_returns)
+        median = jnp.median(train_returns)
+        mean_line = ax.axvline(mean, color="red", label="Mean")
+        median_line = ax.axvline(median, color="green", label="Median")
+
+        # Only store the lines and labels from the first subplot
+        if i == 0:
+            lines.extend([mean_line, median_line])
+            labels.extend(["Mean", "Median"])
+
+    # Add a single legend to the figure
+    fig.legend(lines, labels, loc="center right")
+
     fig.suptitle("Histogram of returns for each task (training)")
     plt.tight_layout()
+    plt.subplots_adjust(right=0.9)
     plt.show()
 
 
